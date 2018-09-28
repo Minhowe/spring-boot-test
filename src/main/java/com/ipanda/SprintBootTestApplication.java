b8eff6e032e3134c109c88adfb28c8b89139787b
Lovell <minhowe@163.com>
2018-09-28 21:24:01 +0800

    Initial commit

---
+++ src/main/java/com/ipanda/SprintBootTestApplication.java
@@ -0,0 +1,12 @@
+package com.ipanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootTestApplication.class, args);
+    }
+}
